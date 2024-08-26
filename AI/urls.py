302a83e51d8090e53ed5928d5912cf76c515b358
Samir Elzer <76245712+samiralaa@users.noreply.github.com>
2024-08-27 00:31:57 +0300

    first commit

--- AI/urls.py
+++ AI/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from AI.views import compare_videos
+
 from AI.views import home
 urlpatterns = [
     path('compare-videos/', compare_videos, name='compare_videos'),
     path('filter-pdf/', compare_videos, name='filter_pdf'),
     path('', home, name='home'), 
+
+
 ]
--- projectname/settings.py
+++ projectname/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+
 import django_heroku
 import os
 import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +30,7 @@ SECRET_KEY = 'django-insecure-on7!p08r6n96_1_rv^h=u4us+nkcdu^*5im6q+6f6(mj^ud@#)
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -121,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
